5b88829c6d9348a25956916bd0a2fc224838df94
Michael Love <mlove@columnit.com>
2019-02-07 10:25:54 -0500

    fix syntax and add if

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,11 +2,15 @@
 PYTHON_INTERPRETER=/usr/bin/python3
 VIRT_DIRECTORY=.virt
 # Create the virtual environment
-$(PYTHON_INTERPRETER) -m venv .$(VIRT_DIRECTORY)
+$PYTHON_INTERPRETER -m venv $VIRT_DIRECTORY
 
 # Source the venv and install the reuqirements
-source $(VIRT_DIRECTORY)/bin/activate
-$(VIRT_DIRECTORY)/bin/pip install -r requirements.txt
+if [ -e requirements.txt]; then 
 
+    source $VIRT_DIRECTORY/bin/activate
+    $VIRT_DIRECTORY/bin/pip install -r requirements.txt
+else
+    echo "No requirements.txt"
+fi
 
 
